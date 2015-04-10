0838de7b9721ff24252d16021c57374c78beb8a4
Michael Battaglia <mbattaglia006@gmail.com>
2015-04-10 18:49:30 -0400

    Added some code to show state structure.

---
+++ Program.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Timers;
+using Microsoft.Kinect;
+
+namespace KinectSkeletonData
+{
+    enum State { Calibrate, Run };
+
+    class Program
+    {
+        public static State state = State.Calibrate;
+        public static KinectSensor sensor;
+        public static Timer timer;
+        public static bool reset = true;
+
+        static void Main(string[] args)
+        {
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            // To make your app robust against plug/unplug, 
+            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != sensor)
+            {
+                // Turn on the skeleton stream to receive skeleton frames
+                sensor.SkeletonStream.Enable();
+
+                // Add an event handler to be called whenever there is new color frame data
+                sensor.SkeletonFrameReady += sensor_SkeletonFrameReady;
+
+                // Start the sensor!
+                try
+                {
+                    sensor.Start();
+                    timer = new Timer(5000);
+                    timer.AutoReset = true;
+                    timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+                    timer.Start();
+                    sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                }
+                catch (IOException)
+                {
+                    sensor = null;
+                }
+            }
+
+            if (null == sensor)
+            {
+                Console.Write("No sensor");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Timer reset");
+            reset = true;
+        }
+
+        static void sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            if (reset)
+            {
+                switch (state)
+                {
+                    case State.Calibrate:
+                        // Do calibrations
+
+                        if (calibaretion_complete)
+                            state = State.Run;
+                        break;
+                    case State.Run:
+                        // Alert user of slouching
+                        break;
+                }
+
+                /*
+                Skeleton[] skeletons = new Skeleton[0];
+
+                using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+                {
+                    if (skeletonFrame != null)
+                    {
+                        skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                        skeletonFrame.CopySkeletonDataTo(skeletons);
+                    }
+                }
+
+                Console.WriteLine(skeletons.Length.ToString() + " sekelton frames:");
+
+                foreach (Skeleton skel in skeletons)
+                {
+                    if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                    {
+                        //Console.WriteLine("tracked");
+                        Joint head = skel.Joints[JointType.Head];
+                        Console.WriteLine(head.Position.X.ToString() + head.Position.Y.ToString() + head.Position.Z.ToString());
+                    }
+                    else if (skel.TrackingState == SkeletonTrackingState.PositionOnly)
+                    {
+                        Console.WriteLine("position only");
+                    }
+                    else
+                        Console.WriteLine("Not tracked");
+                }
+                */
+
+                reset = false;
+            }
+        }
+    }
+}
