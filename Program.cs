9436a48c0f09b4322264572e81ee41e7c775836b
Mike Battaglia <mbattaglia006@gmail.com>
2015-04-11 15:36:59 -0400

    Did some clean up and added states to the program.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,11 @@ using Microsoft.Kinect;
 
 namespace KinectSkeletonData
 {
+    enum State { Calibrate, Run, Test };
+
     class Program
     {
+        public static State state = State.Calibrate;
         public static KinectSensor sensor;
         public static Timer timer;
         public static bool reset = true;
@@ -20,8 +23,6 @@ namespace KinectSkeletonData
         {
             // Look through all sensors and start the first connected one.
             // This requires that a Kinect is connected at the time of app startup.
-            // To make your app robust against plug/unplug, 
-            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
             foreach (var potentialSensor in KinectSensor.KinectSensors)
             {
                 if (potentialSensor.Status == KinectStatus.Connected)
@@ -36,30 +37,28 @@ namespace KinectSkeletonData
                 // Turn on the skeleton stream to receive skeleton frames
                 sensor.SkeletonStream.Enable();
 
-
-
-                // Add an event handler to be called whenever there is new color frame data
+                // Add an event handler to be called whenever there is new skeleton frame data
                 sensor.SkeletonFrameReady += sensor_SkeletonFrameReady;
 
-                // Start the sensor!
+                // Start the sensor and the timer
                 try
                 {
-                    sensor.Start();
-                    timer = new Timer(5000);
+                    timer = new Timer(1000);
                     timer.AutoReset = true;
                     timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
                     timer.Start();
+                    sensor.Start();
                     sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
                 }
                 catch (IOException)
                 {
-                    sensor = null;
+                    Console.Write("Sensor failed to start.");
                 }
             }
 
             if (null == sensor)
             {
-                Console.Write("No sensor");
+                Console.Write("No sensor.");
             }
 
             Console.ReadKey();
@@ -67,59 +66,16 @@ namespace KinectSkeletonData
 
         static void timer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            Console.WriteLine("Timer reset");
             reset = true;
         }
 
         static void sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
-            if (calibrated<3)
+            // We only use the frame if the timer reset (this is to limit the frame rate)
+            if (reset)
             {
-                Skeleton[] calskeletons = new Skeleton[0];
-
-                using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
-                {
-                    if (skeletonFrame != null)
-                    {
-                        calskeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
-                        skeletonFrame.CopySkeletonDataTo(calskeletons);
-                    }
-                }
-                foreach (Skeleton calskel in calskeletons)
-                {
-                    if (calskel.TrackingState == SkeletonTrackingState.Tracked)
-                    {
-                        Joint left = calskel.Joints[JointType.ShoulderLeft];
-                        Joint head = calskel.Joints[JointType.Head];
-                        Joint right = calskel.Joints[JointType.ShoulderRight];
-                        if (calibrated == 0)
-                        {
-                            calibration[0, 0] = left.Position.X;
-                            calibration[0, 1] = left.Position.Y;
-                            calibration[0, 2] = left.Position.Z;
-                        }
-                        else
-                        {
-                            for (int x = 0; x < calibration.GetLength(0); x += 1)
-                            {
-                                for (int y = 0; x < calibration.GetLength(1); y += 1)
-                                {
-
-                                }
-                            }
-                        }
-                    }
-                }
-                calibrated++;
-            }
-
-
-
-
-            else if (reset)
-            {
+                // Here we get all 6 skeletons (even if they are not all being used)
                 Skeleton[] skeletons = new Skeleton[0];
-
                 using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
                 {
                     if (skeletonFrame != null)
@@ -129,24 +85,71 @@ namespace KinectSkeletonData
                     }
                 }
 
-                Console.WriteLine(skeletons.Length.ToString() + " skeleton frames:");
-
-                foreach (Skeleton skel in skeletons)
+                // What we do with the skeleton data is determined by the state the program is in
+                switch (state)
                 {
-                    if (skel.TrackingState == SkeletonTrackingState.Tracked)
-                    {
-                        //Console.WriteLine("tracked");
-                        Joint head = skel.Joints[JointType.Head];
-                        Console.WriteLine(head.Position.X.ToString() + head.Position.Y.ToString() + head.Position.Z.ToString());
-                    }
-                    else if (skel.TrackingState == SkeletonTrackingState.PositionOnly)
-                    {
-                        Console.WriteLine("position only");
-                    }
-                    else
-                        Console.WriteLine("Not tracked");
-                }
+                    // If the program is calibrating, we will get the calibration data from this frame
+                    case State.Calibrate:
+                        if (calibrated < 3)
+                        {
+                            foreach (Skeleton skel in skeletons)
+                            {
+                                if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                                {
+                                    Joint left = skel.Joints[JointType.ShoulderLeft];
+                                    Joint head = skel.Joints[JointType.Head];
+                                    Joint right = skel.Joints[JointType.ShoulderRight];
+                                    if (calibrated == 0)
+                                    {
+                                        calibration[0, 0] = left.Position.X;
+                                        calibration[0, 1] = left.Position.Y;
+                                        calibration[0, 2] = left.Position.Z;
+                                    }
+                                    else
+                                    {
+                                        for (int x = 0; x < calibration.GetLength(0); x += 1)
+                                        {
+                                            for (int y = 0; x < calibration.GetLength(1); y += 1)
+                                            {
 
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            calibrated++;
+                        }
+                        break;
+
+                    // If the program is running, we will determin if the use has good or bad posture
+                    case State.Run:
+                        break;
+
+                    // This state is for testing.  Here we put whatever we want and it will not interfere with the rest of the program.
+                    // Use this state by setting state = State.Testing where it is declared.
+                    case State.Test:
+                        Console.WriteLine(skeletons.Length.ToString() + " skeleton frames:");
+
+                        foreach (Skeleton skel in skeletons)
+                        {
+                            if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                            {
+                                //Console.WriteLine("tracked");
+                                Joint head = skel.Joints[JointType.Head];
+                                Console.WriteLine(head.Position.X.ToString() + head.Position.Y.ToString() + head.Position.Z.ToString());
+                            }
+                            else if (skel.TrackingState == SkeletonTrackingState.PositionOnly)
+                            {
+                                Console.WriteLine("position only");
+                            }
+                            else
+                                Console.WriteLine("Not tracked");
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
                 reset = false;
             }
         }
